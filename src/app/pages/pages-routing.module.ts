9bae4addb4170be9333311515dad4c1466900c97
vitorfranca9 <vitorfranca9@gmail.com>
2018-11-18 23:20:18 -0200

    Share

---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,47 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NotFoundComponent } from './miscellaneous/not-found/not-found.component';
+
+const routes: Routes = [{
+  path: '',
+  component: PagesComponent,
+  children: [{
+    path: 'iot-dashboard',
+    component: DashboardComponent,
+  }, {
+    path: 'ui-features',
+    loadChildren: './ui-features/ui-features.module#UiFeaturesModule',
+  }, {
+    path: 'components',
+    loadChildren: './components/components.module#ComponentsModule',
+  }, {
+    path: 'charts',
+    loadChildren: './charts/charts.module#ChartsModule',
+  }, {
+    path: 'forms',
+    loadChildren: './forms/forms.module#FormsModule',
+  }, {
+    path: 'tables',
+    loadChildren: './tables/tables.module#TablesModule',
+  }, {
+    path: 'miscellaneous',
+    loadChildren: './miscellaneous/miscellaneous.module#MiscellaneousModule',
+  }, {
+    path: '',
+    redirectTo: 'iot-dashboard',
+    pathMatch: 'full',
+  }, {
+    path: '**',
+    component: NotFoundComponent,
+  }],
+}];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PagesRoutingModule {
+}
